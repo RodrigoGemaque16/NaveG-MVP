4ba6014e090b2a377102a68ca77642a031f7f9d2
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-22 19:29:54 -0300

    route site root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   devise_for :admins
   devise_for :users
   namespace :users_backoffice do
@@ -7,5 +10,6 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'site/welcome#index'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
