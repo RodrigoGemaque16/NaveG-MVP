d207d10cd0225ec356df35d52c4a01be22f15821
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-26 16:10:37 -0300

    routes for users and admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
+  namespace :admins_backoffice do
+    get 'users/index'
+  end
   namespace :site do
     get 'welcome/index'
   end
   devise_for :admins
   devise_for :users
+
   namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
+    resources :users
+    resources :admins
   end
 
   root to: 'site/welcome#index'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
