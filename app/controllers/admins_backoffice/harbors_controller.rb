71d1508897b07fe4e1fd3073ccf8bd120d6250f8
RodrigoGemaque <ro.gemaque16@gmail.com>
2021-01-28 11:22:12 -0300

    Controller harbor with crud actions

---
+++ app/controllers/admins_backoffice/harbors_controller.rb
@@ -0,0 +1,62 @@
+class AdminsBackoffice::HarborsController < AdminsBackofficeController
+  before_action :verify_fields, only: [:update]
+  before_action :set_harbor, only: [:update,:edit]
+
+  def index
+    @harbors = Harbor.all
+  end
+
+  def new
+    @harbor = Harbor.new
+  end
+
+  def create
+    @harbor = Harbor.new(params_harbor)
+    if @harbor.save
+      flash[:success] = "Object successfully created"
+      
+      redirect_to admins_backoffice_harbors_path
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end  
+
+
+  def edit; end
+  def update
+    if @harbor.update(params_harbor)
+        redirect_to admins_backoffice_harbors_path, notice: "Usuario atualizado com sucesso!"
+        else
+          render :edit
+      end
+  end
+
+
+  def destroy 
+    set_harbor
+    if @harbor.destroy
+      flash[:success] = 'Object was successfully deleted.'
+      redirect_to admins_backoffice_harbors_path
+    else
+      flash[:error] = 'Something went wrong'
+      render :index
+    end
+  end
+
+  private
+  def params_harbor
+      params.require(:harbor).permit(:name, :city,:state)
+  end
+
+  def verify_fields
+    if params[:harbor][:name].blank? && params[:harbor][:city].blank? && params[:harbor][:state].blank?
+      params[:harbor].extract!(:name,:city,:state)
+    end   
+  end
+
+  def set_harbor 
+    @harbor = Harbor.find(params[:id])
+  end  
+
+end
