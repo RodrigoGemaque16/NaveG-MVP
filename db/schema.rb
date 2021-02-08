b344db0f3d9347bf748e5259f53eb75273b255c7
Rodrigo <ro.gemaque16@gmail.com>
2021-02-08 20:25:10 -0300

    harbor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_105747) do
+ActiveRecord::Schema.define(version: 2021_02_04_231546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2021_02_04_105747) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "harbors", force: :cascade do |t|
+    t.string "name"
+    t.bigint "city_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["city_id"], name: "index_harbors_on_city_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_02_04_105747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "harbors", "cities"
 end
