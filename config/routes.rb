af061fe97c0aef44cc3976882ce93366e74133bf
Rodrigo <ro.gemaque16@gmail.com>
2021-02-09 07:17:16 -0300

    rest routes for cities

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     resources :users
     resources :admins
     resources :harbors
+    resources :cities
   end
 
   root to: 'site/welcome#index'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
